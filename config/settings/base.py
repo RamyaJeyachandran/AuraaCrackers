091f8cbb09be7eec358bb020e045658c9a9369ca
unknown <ramyaworkac@gmail.com>
2026-05-16 10:33:56 +0530

    Branding: Updated footer text and fixed tooltip z-index/clipping issues

--- config/settings/base.py
+++ config/settings/base.py
@@ -115,7 +115,7 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Media files
-MEDIA_URL = "media/"
+MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 # Default primary key field type
